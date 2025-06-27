aa341f16eb085a2948de3029c8b553cb57f01d40
Russel <russelmperera123@gmail.com>
2025-06-27 15:24:23 +0530

    increase upload limit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.config.update(
     UPLOAD_FOLDER='uploads',
-    MAX_CONTENT_LENGTH=16 * 1024 * 1024,
+    MAX_CONTENT_LENGTH=500 * 1024 * 1024,  # 500 MB
     SECRET_KEY=os.getenv('FLASK_SECRET_KEY', 'change_me')
 )
 
